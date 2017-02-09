75b98fefdfa2ae19e31a24df7f7b80dfb06ad567
David Yi <dy89@njit.edu>
2017-02-08 20:24:26 -0500

    revised discount display

--- display_discount.php
+++ display_discount.php
@@ -1,4 +1,18 @@
 <!DOCTYPE html>
+<?php
+	//get data from form
+	$product_description = filter_input(INPUT_POST, 'product_description');
+	$list_price = filter_input(INPUT_POST, 'list_price');
+	$discount_percent = filter_input(INPUT_POST, 'discount_percent');
+	//calculate discount and discount price
+	$discount = $list_price * $discount_percent * 0.01;
+	$discount_price = $list_price - $discount;
+	// apply format
+	$list_price_f = "$".number_format($list_price, 2);
+	$discount_percent_f = $discount_percent."%";
+	$discount_f = "$".number_format($discount, 2);
+	$discount_price_f = "$".number_format($discount_price, 2);
+?>
 <html>
 <head>
     <title>Product Discount Calculator</title>
@@ -6,22 +20,22 @@
 </head>
 <body>
     <main>
-        <h1>This page is under construction</h1>
-
+        <h1>Product Discount Calculator</h1>
+	        
         <label>Product Description:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo htmlspecialchars($product_description); ?></span><br>
 
         <label>List Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo htmlspecialchars($list_price_f); ?></span><br>
 
         <label>Standard Discount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo htmlspecialchars($discount_percent_f); ?></span><br>
 
         <label>Discount Amount:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo htmlspecialchars($discount_f); ?></span><br>
 
         <label>Discount Price:</label>
-        <span><?php echo ''; ?></span><br>
+        <span><?php echo htmlspecialchars($discount_price_f); ?></span><br>
     </main>
 </body>
 </html>
